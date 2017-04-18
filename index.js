f95fb4d505072402c9f6199197127be260fa73bf
Jonathan Hollinger <jmholl5@gmail.com>
2017-04-18 17:28:17 -0400

    Update

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/reports/parcel/:parcelid', function (req, res) {
                           "account_status": data[i].account_status,
                           "charge_date": moment(data[i].charge_date).format('M-D-YYYY'),
                           "billed_consump": data[i].billed_consump,
-                          "adjustment_date": moment(data[i].adjustment_date).format('M-D-YYYY'),
+                          "adjustment_date": formatDate(data[i].adjustment_date,'M-D-YYYY'),
                           "consump_adj": data[i].consump_adj
                           }
 
@@ -66,7 +66,7 @@ app.get('/reports/parcel/:parcelid', function (req, res) {
 app.get('/api/serviceaddress/:address', function (req, res) {
       pg.connect(process.env.HEROKU_POSTGRESQL_CYAN_URL, function(err, client, done) {
             client.query({
-                    text: 'SELECT kawc.address, kawc.unit, kawc.parcelid, kawc.kawc_premise_id, water_bills.name, water_bills.account_status, water_bills.charge_date, water_bills.billed_consump, water_bills.adjustment_date, water_bills.consump_adj,kawc.lat, kawc.lng FROM kawc INNER JOIN water_bills on kawc.kawc_premise_id = water_bills.kawc_premise_id WHERE kawc.address = $1 ORDER BY charge_date DESC',
+                    text: 'SELECT kawc.address, kawc.unit, kawc.parcelid, kawc.kawc_premise_id, water_bills.name, water_bills.account_status, water_bills.charge_date, water_bills.billed_consump, water_bills.adjustment_date, water_bills.consump_adj,kawc.lat, kawc.lng FROM kawc INNER JOIN water_bills on kawc.kawc_premise_id = water_bills.kawc_premise_id WHERE kawc.address = $1 ORDER BY charge_date ASC',
                     values: [req.params.address]
                 },function(err, result) {
                     done();
@@ -99,3 +99,13 @@ app.get('/api/noservice/:length', function (req, res) {
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+
+//Helper Functions
+function formatDate(input, format){
+if (input) {
+  return moment(input).format(format)
+}
+else {
+  return ''
+}
