6faf955bb6e174824adfdb6ba798337533dd49ce
Jonathan Hollinger <jmholl5@gmail.com>
2017-04-17 20:56:12 -0400

    Update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 app.get('/api/serviceaddress/:address', function (req, res) {
       pg.connect(process.env.DATABASE_URL, function(err, client, done) {
             client.query({
-                    text: 'SELECT KAWC.Address, KAWC.Unit, KAWC.Parcelid, KAWC.KAWC_Premise_Id, WATER_BILLS.Name, WATER_BILLS.Account_Status, WATER_BILLS.Charge_Date, WATER_BILLS.Billed_Consump, WATER_BILLS.Adjustment_Date, WATER_BILLS.Consump_Adj,KAWC.Lat, KAWC.Lng FROM KAWC INNER JOIN WATER_BILLS ON KAWC.KAWC_PREMISE_ID = WATER_BILLS.KAWC_PREMISE_ID WHERE KAWC.address = $1 ORDER BY CHARGE_DATE DESC',
+                    text: 'SELECT kawc.address, kawc.unit, kawc.parcelid, kawc.kawc_premise_id, water_bills.name, water_bills.account_status, water_bills.charge_date, water_bills.billed_consump, water_bills.adjustment_date, water_bills.consump_adj,kawc.lat, kawc.lng FROM kawc INNER JOIN water_bills on kawc.kawc_premise_id = water_bills.kawc_premise_id WHERE kawc.address = $1 ORDER BY charge_date desc',
                     values: [req.params.address]
                 },function(err, result) {
                     done();
