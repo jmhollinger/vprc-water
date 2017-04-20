82d0613c7d636c7ce0cb2cbff0468e0381bcfd6f
Jonathan Hollinger <jmholl5@gmail.com>
2017-04-20 09:00:11 -0400

    Allow CORS

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var moment = require('moment');
 
 var app = express()
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static('public'));
 
 app.set('view engine', 'pug');
@@ -33,7 +39,7 @@ app.get('/api/address_search/:query', function (req, res) {
                         res.json(
                           {
                             status : 'success',
-                            error: result.rows
+                            results: result.rows
                           }) 
 
                         }
